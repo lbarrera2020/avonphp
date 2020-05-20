a672f3b88c5682bff1af1c08cf7836a8dba3acc3
Luis <lbarrerac@gmail.com>
2020-05-20 11:19:01 -0600

    paso a español los textos

--- index.php
+++ index.php
@@ -284,14 +284,14 @@
 
         <!-- Login Form -->
         <form>
-            <input type="text" id="login" class="fadeIn second" name="login" placeholder="login">
+            <input type="text" id="login" class="fadeIn second" name="login" placeholder="usuario">
             <input type="text" id="password" class="fadeIn third" name="login" placeholder="password">
             <input type="submit" class="fadeIn fourth" value="Log In">
         </form>
 
         <!-- Remind Passowrd -->
         <div id="formFooter">
-            <a class="underlineHover" href="#">Forgot Password?</a>
+            <a class="underlineHover" href="#">¿Se te olvidó tu contraseña?</a>
         </div>
 
     </div>
