85cda384d1cc9bafddf7ba0acc2279bfbffea9f1
Luis <lbarrerac@gmail.com>
2020-05-20 22:04:28 -0600

    Se agrega el color pink al boostramp

--- UsuarioNuevo.php
+++ UsuarioNuevo.php
@@ -2,12 +2,10 @@
 <html lang="en">
 <head>
     <title>Prueba AVON</title>
-    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" id="bootstrap-css">
+<!--    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" id="bootstrap-css">-->
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css" id="bootstrap-css">
     <script type="text/javascript" src="js/bootstrap.min.js"></script>
     <script type="text/javascript" src="js/jquery-3.5.1.min.js"></script>
-<!--    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">-->
-<!--    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>-->
-<!--    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>-->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
 </head>
 <body>
@@ -21,7 +19,7 @@
             <form action="CrearUsuarioValidado.php" method="post">
                 <div class="card border-primary rounded-0">
                     <div class="card-header p-0">
-                        <div class="bg-info text-white text-center py-2">
+                        <div class="bg-pink text-white text-center py-2">
                             <h3><i class="fa fa-user"></i> Usuario Nuevo</h3>
 <!--                            <p class="m-0"></p>-->
                         </div>
@@ -32,7 +30,7 @@
                         <div class="form-group">
                             <div class="input-group mb-2">
                                 <div class="input-group-prepend">
-                                    <div class="input-group-text"><i class="fa fa-user text-info"></i></div>
+                                    <div class="input-group-text"><i class="fa fa-user text-pink"></i></div>
                                 </div>
                                 <input type="text" class="form-control" id="nombre" name="nombre" placeholder="Nombre y Apellido" required>
                             </div>
@@ -40,7 +38,7 @@
                         <div class="form-group">
                             <div class="input-group mb-2">
                                 <div class="input-group-prepend">
-                                    <div class="input-group-text"><i class="fa fa-user text-info"></i></div>
+                                    <div class="input-group-text"><i class="fa fa-user text-pink"></i></div>
                                 </div>
                                 <input type="text" class="form-control" id="usuario" name="usuario" placeholder="Usuario" required>
                             </div>
@@ -48,7 +46,7 @@
                         <div class="form-group">
                             <div class="input-group mb-2">
                                 <div class="input-group-prepend">
-                                    <div class="input-group-text"><i class="fa fa-key text-info"></i></div>
+                                    <div class="input-group-text"><i class="fa fa-key text-pink"></i></div>
                                 </div>
                                 <input type="password" class="form-control" id="clave" name="clave" placeholder="Clave Temporal" required>
                             </div>
@@ -56,7 +54,7 @@
                         <div class="form-group">
                             <div class="input-group mb-2">
                                 <div class="input-group-prepend">
-                                    <div class="input-group-text"><i class="fa fa-envelope text-info"></i></div>
+                                    <div class="input-group-text"><i class="fa fa-envelope text-pink"></i></div>
                                 </div>
                                 <input type="email" class="form-control" id="correo" name="correo" placeholder="ejemplo@gmail.com" required>
                             </div>
@@ -64,7 +62,7 @@
                         <div class="form-group">
                             <div class="input-group mb-2">
                                 <div class="input-group-prepend">
-                                    <div class="input-group-text"><i class="fa fa-mobile-alt text-info"></i></div>
+                                    <div class="input-group-text"><i class="fa fa-mobile-alt text-pink"></i></div>
                                 </div>
                                 <input type="tel" class="form-control" id="telcel" name="telcel" placeholder="Telefono Celular" required>
                             </div>
@@ -72,7 +70,7 @@
                         <div class="form-group">
                             <div class="input-group mb-2">
                                 <div class="input-group-prepend">
-                                    <div class="input-group-text"><i class="fa fa-phone text-info"></i></div>
+                                    <div class="input-group-text"><i class="fa fa-phone text-pink"></i></div>
                                 </div>
                                 <input type="tel" class="form-control" id="telfijo" name="telfijo" placeholder="Telefono Fijo">
                             </div>
@@ -80,7 +78,7 @@
                         <div class="form-group">
                             <div class="input-group mb-2">
                                 <div class="input-group-prepend">
-                                    <div class="input-group-text"><i class="fa fa-address-card text-info"></i></div>
+                                    <div class="input-group-text"><i class="fa fa-address-card text-pink"></i></div>
                                 </div>
                                 <input type="text" class="form-control" id="direccion1" name="direccion1" placeholder="Direccion" required>
                             </div>
@@ -88,7 +86,7 @@
                         <div class="form-group">
                             <div class="input-group mb-2">
                                 <div class="input-group-prepend">
-                                    <div class="input-group-text"><i class="fa fa-address-card text-info"></i></div>
+                                    <div class="input-group-text"><i class="fa fa-address-card text-pink"></i></div>
                                 </div>
                                 <input type="hidden" name="ref" id="ref" value="1">
                                 <input type="text" class="form-control" id="direccion2" name="direccion2" placeholder="Direccion (Opcional)">
@@ -96,7 +94,7 @@
                         </div>
 
                         <div class="text-center">
-                            <input type="submit" value="Enviar" class="btn btn-info btn-block rounded-0 py-2">
+                            <input type="submit" value="Enviar" class="btn btn-pink btn-block rounded-0 py-2">
                         </div>
                     </div>
 
